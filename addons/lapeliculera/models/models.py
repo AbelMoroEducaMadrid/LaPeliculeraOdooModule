927b44208366deea174d385b8fe9c71416980bef
Abel Moro <abel.moro@educa.madrid.org>
2025-02-27 17:19:18 +0100

    String to string

--- addons/lapeliculera/models/models.py
+++ addons/lapeliculera/models/models.py
@@ -6,22 +6,22 @@ class lapeliculera_pelicula(models.Model):
     _name = 'lapeliculera.pelicula'
     _description = 'Película'
 
-    name = fields.Char(String="Título", required=True, help="Introduce el título de la película")
-    director = fields.Char(String="Director", required=True, help="Introduce el director de la película")
-    color = fields.Boolean(String="Color", help="Indica si la película es en color")
-    duracion = fields.Integer(String="Duración en minutos", help="Indica la duración de la película en minutos")
-    industria = fields.Selection([('hollywood', 'Hollywood'), ('europea', 'Europea'), ('bollywood', 'Bollywood'), ('otra', 'Otra')], String="Industria", required=True, help="Indica la industria de la película")
-    fecha = fields.Date(String="Fecha de estreno en españa", help="Indica la fecha de estreno en España de la película")
-    sinopsis = fields.Text(String="sinopsis", help="Introduce la sinopsis de la película")
-    genero = fields.Many2one('lapeliculera.genero', String="Género", required=True, help="Indica el género de la película")
+    name = fields.Char(string="Título", required=True, help="Introduce el título de la película")
+    director = fields.Char(string="Director", required=True, help="Introduce el director de la película")
+    color = fields.Boolean(string="Color", help="Indica si la película es en color")
+    duracion = fields.Integer(string="Duración en minutos", help="Indica la duración de la película en minutos")
+    industria = fields.Selection([('hollywood', 'Hollywood'), ('europea', 'Europea'), ('bollywood', 'Bollywood'), ('otra', 'Otra')], string="Industria", required=True, help="Indica la industria de la película")
+    fecha = fields.Date(string="Fecha de estreno en españa", help="Indica la fecha de estreno en España de la película")
+    sinopsis = fields.Text(string="sinopsis", help="Introduce la sinopsis de la película")
+    genero = fields.Many2one('lapeliculera.genero', string="Género", required=True, help="Indica el género de la película")
     portada = fields.Binary(string="Portada", help="Sube la imagen de la película")
 
 class lapeliculera_genero(models.Model):
     _name = 'lapeliculera.genero'
     _description = 'Género cinematográfico'
-    name = fields.Char(String="Género", required=True, help="Introduce el género cinematográfico")
-    comentario = fields.Text(String="Comentarios", help="Introduce comentarios sobre el género") 
-    pelicula = fields.One2many('lapeliculera.pelicula', 'genero', String="Películas", help="Indica las películas de este género")
+    name = fields.Char(string="Género", required=True, help="Introduce el género cinematográfico")
+    comentario = fields.Text(string="Comentarios", help="Introduce comentarios sobre el género") 
+    pelicula = fields.One2many('lapeliculera.pelicula', 'genero', string="Películas", help="Indica las películas de este género")
     
 class Reparto(models.Model):
     _name = 'lapeliculera.reparto'
